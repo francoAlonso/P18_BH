9a9e6ee3a352cacb0c558506fbeb3dfabdb4e550
Leandro Leibovich <leiboleo@gmail.com>
2015-09-12 17:53:26 -0300

    instrucciones crear partida

--- api/controller/PartidaController.php
+++ api/controller/PartidaController.php
@@ -1,5 +1,15 @@
 <?php
 Class PartidaController{
-	
+	public static function CrearPartida($pdo, $ID_Usuario)
+	{
+		// Buscar partida disponible (habilitado, y que cantidad de registros Partida_Usuario para esa partida sea menor que cantidad de usuarios de la partida
+		// Si está disponible, agregar Partida_Usuario y devolver la lista de preguntas y respuestas.
+		// sino hacer lo que sigue (todo): Partida -> CrearPartida
+		// Partida_UsuarioController -> AgregarUsuarioAPartida
+		// Declarar variable array para contener las preguntas con sus respuestas
+		// {partida: datosPartida, [Pregunta: [{"ID":"2","ID_Nivel":"1","Texto":"10?","Habilitado":"1"}], Respuestas: [[{"ID":"3","ID_Pregunta":"2","Texto":"10","Habilitado":"1"},{"ID":"4","ID_Pregunta":"2","Texto":"20","Habilitado":"1"},{"ID":"5","ID_Pregunta":"2","Texto":"30","Habilitado":"1"},{"ID":"6","ID_Pregunta":"2","Texto":"30","Habilitado":"1"}]]]
+		// for/while hasta Partida->Cantidad_Preguntas, agregar las preguntas a Partida_Pregunta y guardar la preguntas con sus respuestas al array anterior. VERIFICAR QUE NO SE REPITAN
+		// Devolver la partida con sus preguntas y respuestas
+	}
 }
 ?>
--- api/model/Partida.php
+++ api/model/Partida.php
@@ -18,7 +18,7 @@ class Partida
 				');
 		$statement->execute($params);
 		$statement->setFetchMode(PDO::FETCH_CLASS, 'Partida');
-		return $statement->fetchAll(); // fetch trae uno sólo (o debe iterarse). fetchAll trae todos los registros.
+		return $statement->fetchAll(); // fetch trae uno sï¿½lo (o debe iterarse). fetchAll trae todos los registros.
 	}
 	public static function ObtenerPorId($id, $pdo)
 	{
@@ -34,5 +34,20 @@ class Partida
 		return $statement->fetch();
 	}
 	
+	public static function CrearPartida($pdo){
+		$pdo->beginTransaction();
+		$now = new DateTime();
+		$params = array(':Habilitado' => 1, ':Fecha_Inicio' => $now->format('Y-m-d H:i:s'), ':Fecha_Fin' => null, ':Cantidad_Usuario' => 2, ':Cantidad_Preguntas' => 10);
+		$statement = $pdo->prepare('INSERT INTO Partida (Habilitado, Fecha_Inicio, Fecha_Fin, Cantidad_Usuario, Cantidad_Preguntas)
+									VALUES (:Habilitado, :Fecha_Inicio, :Fecha_Fin, :Cantidad_Usuario, :Cantidad_Preguntas)');
+		$statement->execute($params);
+		$idPartida = $pdo->lastInsertId();
+		$partida = Partida::ObtenerPorId($idPartida, $pdo);
+		$pdo->commit();
+		return $usuario;
+	}
+	public static function ObtenerPartidaDisponible($ID_Usuario, $pdo){
+		
+	}
 }
 ?>
