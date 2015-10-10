923f6cfd3bff2fc950aa2ecc23219ba0a142d9dc
Leandro Leibovich <leiboleo@gmail.com>
2015-10-10 15:54:20 -0300

    visualizacion puntajes gerencias
    El resultado se envia en decimal, convertirlo a entero en front end

--- api/model/Puntaje_Gerencia.php
+++ api/model/Puntaje_Gerencia.php
@@ -9,10 +9,12 @@ class Puntaje_Gerencia
 	{
 		$params = array();
 		$statement = $pdo->prepare('
-				SELECT G.ID
-						, G.Nombre
-						, SUM(COALESCE(U.Puntaje, 0)) AS Puntaje
-						, SUM((100-(SUM(U.ID)/(SELECT SUM(*) FROM Usuario)*100))*(SUM(U.Puntaje))) AS Puntaje2
+				 SELECT G.ID
+					, G.Nombre
+					/*, SUM(COALESCE(U.Puntaje, 0)) AS Puntaje
+					, SUM((100-(COUNT(U.ID)/(SELECT COUNT(ID) FROM Usuario)*100))*(SUM(U.Puntaje))) AS Puntaje2*/
+				    , ((100-(COUNT(U.ID) / (SELECT COUNT(ID) FROM Usuario)*100)) * SUM(U.Puntaje)) AS Puntaje
+				    /*, (100-(COUNT(U.ID) / (SELECT COUNT(ID) FROM Usuario)*100)) AS PorcentualUsuarios*/
 				FROM Gerencia G
 				LEFT JOIN Usuario U ON G.ID = U.ID_Gerencia
 				GROUP BY G.ID, G.Nombre
